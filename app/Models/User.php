aea582a45813797b3074b89187bf2b4604be3ac1
Adi Arta Wibawa <75978314+adiartawibawa@users.noreply.github.com>
2024-08-02 10:25:05 +0800

    adding comment in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,10 @@ class User extends Authenticatable implements HasMedia, FilamentUser, HasAvatar,
         'password' => 'hashed',
     ];
 
+    /**
+     * Define the sluggable configuration for the model.
+     * The 'username' slug is created from the 'name' attribute.
+     */
     public function sluggable(): array
     {
         return [
@@ -69,43 +73,74 @@ class User extends Authenticatable implements HasMedia, FilamentUser, HasAvatar,
         ];
     }
 
-
+    /**
+     * Get the Filament display name for the user.
+     * If 'username' is set, return it, otherwise return 'name'.
+     */
     public function getFilamentName(): string
     {
         return $this->username ?: $this->name;
     }
 
-    // Define an accessor for the 'name' attribute
+    /**
+     * Accessor for the 'name' attribute.
+     * Combines 'firstname' and 'lastname' into a single string.
+     */
     public function getNameAttribute()
     {
         return "{$this->firstname} {$this->lastname}";
     }
 
+    /**
+     * Determine if the user can access the specified Filament panel.
+     * Always returns true, granting access to all panels.
+     */
     public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
 
+    /**
+     * Get the URL for the user's avatar.
+     * Returns the URL of the first media item in the 'avatars' collection,
+     * or a default avatar URL if no media is found.
+     */
     public function getFilamentAvatarUrl(): ?string
     {
         return $this->getMedia('avatars')?->first()?->getUrl() ?? $this->defaultAvatar();
     }
 
+    /**
+     * Check if the user has the 'super_admin' role.
+     * Uses the role name defined in the configuration.
+     */
     public function isSuperAdmin(): bool
     {
         return $this->hasRole(config('filament-shield.super_admin.name'));
     }
 
+    /**
+     * Check if the user has the 'admin' role.
+     */
     public function isAdmin(): bool
     {
         return $this->hasRole('admin');
     }
 
+    /**
+     * Accessor for the 'avatar_url' attribute.
+     * Returns the URL of the last media item in the 'avatars' collection,
+     * or a default avatar URL if no media is found.
+     */
     public function getAvatarUrlAttribute(): ?string
     {
         return $this->getMedia('avatars')?->last()?->getUrl() ?? $this->defaultAvatar();
     }
 
+    /**
+     * Get the default avatar URL.
+     * Uses Gravatar with the user's email.
+     */
     private function defaultAvatar(): ?string
     {
         return 'https://gravatar.com/avatar/' . md5(strtolower(trim($this->email))) . '?d=mp';
